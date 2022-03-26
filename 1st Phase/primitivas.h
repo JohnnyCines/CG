37dd0133ed3022c21d9f8269742d03a795053f25
JohnnyCines <62761439+JohnnyCines@users.noreply.github.com>
2022-03-26 17:43:03 +0000

    Delete primitivas.h

--- 1st Phase/primitivas.h
+++
@@ -1,4 +0,0 @@
-void plano(float length, float divisions, char* fname);
-void caixa(float dimension, float divisions, char* fname);
-void esfera(float r, int slices, int stacks, char* fname);
-void cone(float radius, float height, int slices, int stacks, char* fname);
